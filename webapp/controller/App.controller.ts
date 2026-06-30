110a303f0ec0996ae065b911eafa084cd9ee3d0a
Andrew Ignacio <andrewignacio@outlook.com.br>
2026-06-30 10:22:21 -0300

    Step 06: Modules

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,10 +1,11 @@
 import Controller from "sap/ui/core/mvc/Controller";
+import MessageToast from "sap/m/MessageToast";
 
 /**
  * @name ui5.walkthrough.controller.App
  */
 export default class AppController extends Controller {
     onShowHello(): void {
-        alert("Hello world");
+        MessageToast.show("Hello world");
     }
 }
