b7c189e77f35a29fb1a44267fb60a6d992753af1
Andrew Ignacio <andrewignacio@outlook.com.br>
2026-06-30 11:40:59 -0300

    Step 10: Manifest

--- webapp/Component.ts
+++ webapp/Component.ts
@@ -2,14 +2,14 @@ import Control from "sap/ui/core/Control";
 import XMLView from "sap/ui/core/mvc/XMLView";
 import UIComponent from "sap/ui/core/UIComponent";
 import JSONModel from "sap/ui/model/json/JSONModel";
-import ResourceModel from "sap/ui/model/resource/ResourceModel";
 
 /**
  * @namespace ui5.walkthrough
  */
 export default class Component extends UIComponent {
     public static metadata = {
-        "interfaces": ["sap.ui.core.IAsyncContentCreation"]
+        "interfaces": ["sap.ui.core.IAsyncContentCreation"],
+        "manifest": "json"
     }
 
     init(): void {
@@ -23,19 +23,5 @@ export default class Component extends UIComponent {
 
         const dataModel = new JSONModel(data);
         this.setModel(dataModel);
-
-        const i18nModel = new ResourceModel({
-            "bundleName": "ui5.walkthrough.i18n.i18n",
-            "supportedLocales": [""],
-            "fallbackLocale": ""
-        });
-        this.setModel(i18nModel, "i18n");
-    }
-
-    createContent(): Control | Promise<Control | null> | null {
-        return XMLView.create({
-            "viewName": "ui5.walkthrough.view.App",
-            "id": "app"
-        });
     }
 }
