f0e69324d3e1b37c65d8439294485ce3b9944472
Andrew Ignacio <andrewignacio@outlook.com.br>
2026-06-30 11:26:24 -0300

    Step 09: Component Configuration

---
+++ webapp/Component.ts
@@ -0,0 +1,41 @@
+import Control from "sap/ui/core/Control";
+import XMLView from "sap/ui/core/mvc/XMLView";
+import UIComponent from "sap/ui/core/UIComponent";
+import JSONModel from "sap/ui/model/json/JSONModel";
+import ResourceModel from "sap/ui/model/resource/ResourceModel";
+
+/**
+ * @namespace ui5.walkthrough
+ */
+export default class Component extends UIComponent {
+    public static metadata = {
+        "interfaces": ["sap.ui.core.IAsyncContentCreation"]
+    }
+
+    init(): void {
+        super.init();
+
+        const data = {
+            recipient: {
+                name: "World"
+            }
+        };
+
+        const dataModel = new JSONModel(data);
+        this.setModel(dataModel);
+
+        const i18nModel = new ResourceModel({
+            "bundleName": "ui5.walkthrough.i18n.i18n",
+            "supportedLocales": [""],
+            "fallbackLocale": ""
+        });
+        this.setModel(i18nModel, "i18n");
+    }
+
+    createContent(): Control | Promise<Control | null> | null {
+        return XMLView.create({
+            "viewName": "ui5.walkthrough.view.App",
+            "id": "app"
+        });
+    }
+}
--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -8,20 +8,6 @@ import ResourceBundle from "sap/base/i18n/ResourceBundle";
  * @name ui5.walkthrough.controller.App
  */
 export default class AppController extends Controller {
-    onInit(): void {
-        const data = {
-            recipient: {
-                name: "World"
-            }
-        }
-
-        const dataModel = new JSONModel(data);
-        this.getView()?.setModel(dataModel);
-
-        const i18nModel = new ResourceModel({ bundleName: "ui5.walkthrough.i18n.i18n" });
-        this.getView()?.setModel(i18nModel, "i18n");
-    }
-
     onShowHello(): void {
         const recipient = (this.getView()?.getModel() as JSONModel)?.getProperty("/recipient/name");
         const resourceBundle = (this.getView()?.getModel("i18n") as ResourceModel)?.getResourceBundle() as ResourceBundle;
