a0fbcb49037d8b5c35dcc156fbba4555fc47ff0e
Andrew Ignacio <andrewignacio@outlook.com.br>
2026-07-03 09:57:19 -0300

    Step 32: Routing Back and History

--- webapp/controller/Detail.controller.ts
+++ webapp/controller/Detail.controller.ts
@@ -1,4 +1,5 @@
 import Controller from "sap/ui/core/mvc/Controller";
+import History from "sap/ui/core/routing/History";
 import Route, { Route$PatternMatchedEvent } from "sap/ui/core/routing/Route";
 import UIComponent from "sap/ui/core/UIComponent";
 
@@ -13,5 +14,18 @@ export default class Detail extends Controller {
             path: "/" + window.decodeURIComponent((event.getParameter("arguments") as any).invoicePath),
             model: "invoice",
         });
+    }
+
+    onNavBack(): void {
+        const history = History.getInstance();
+        const previousPageHash = history.getPreviousHash();
+
+        if (previousPageHash) {
+            window.history.go(-1);
+            return;
+        }
+
+        const router = UIComponent.getRouterFor(this);
+        router.navTo("overview");
     }
 }
