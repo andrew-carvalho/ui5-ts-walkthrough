5944df736bd8d8260ee5eb551f46787a4f6b0d29
Andrew Ignacio <andrewignacio@outlook.com.br>
2026-07-03 10:57:47 -0300

    Step 33: Custom Controls

--- webapp/controller/Detail.controller.ts
+++ webapp/controller/Detail.controller.ts
@@ -2,6 +2,10 @@ import Controller from "sap/ui/core/mvc/Controller";
 import History from "sap/ui/core/routing/History";
 import Route, { Route$PatternMatchedEvent } from "sap/ui/core/routing/Route";
 import UIComponent from "sap/ui/core/UIComponent";
+import ProductRating, { ProductRating$ChangeEvent } from "../control/ProductRating";
+import ResourceModel from "sap/ui/model/resource/ResourceModel";
+import ResourceBundle from "sap/base/i18n/ResourceBundle";
+import MessageToast from "sap/m/MessageToast";
 
 export default class Detail extends Controller {
     onInit(): void {
@@ -10,6 +14,7 @@ export default class Detail extends Controller {
     }
 
     onObjectMatched(event: Route$PatternMatchedEvent): void {
+        (this.byId("rating") as ProductRating).reset();
         this.getView()?.bindElement({
             path: "/" + window.decodeURIComponent((event.getParameter("arguments") as any).invoicePath),
             model: "invoice",
@@ -27,5 +32,14 @@ export default class Detail extends Controller {
 
         const router = UIComponent.getRouterFor(this);
         router.navTo("overview");
+    }
+
+    onRatingChange(event: ProductRating$ChangeEvent): void {
+        const value = event.getParameter("value");
+        const resourceBundle = (
+            this?.getView()?.getModel("i18n") as ResourceModel
+        )?.getResourceBundle() as ResourceBundle;
+
+        MessageToast.show(resourceBundle.getText("ratingConfirmation", [value]) as string);
     }
 }
